da216334c3e3cf65a00da2858508a0e04b4fd318
Owanikin <oderindeife@gmail.com>
2022-09-30 02:23:03 +0100

    test: Mint NFT

--- test/unit/basicNft.test.js
+++ test/unit/basicNft.test.js
@@ -1,26 +1,39 @@
-const { inputToConfig } = require("@ethereum-waffle/compiler");
-const { assert } = require("chai");
-const { network, ethers, deployments } = require("hardhat");
-const { developmentChains } = require("../../helper-hardhat-config");
+const { assert } = require("chai")
+const { network, ethers, deployments } = require("hardhat")
+const { developmentChains } = require("../../helper-hardhat-config")
 
-!developmentChains.includes(network.name) ? describe.skip : describe("Basic NFT Unit Tests", function () {
-    let basicNft, deployer
+!developmentChains.includes(network.name)
+    ? describe.skip
+    : describe("Basic NFT Unit Tests", function () {
+          let basicNft, deployer
 
-    beforeEach(async () => {
-        accounts = await ethers.getSigners()
-        deployer = accounts[0]
-        await deployments.fixture(["basicnft"])
-        basicNft = await ethers.getContract("BasicNft")
-    })
+          beforeEach(async () => {
+              accounts = await ethers.getSigners()
+              deployer = accounts[0]
+              await deployments.fixture(["BasicNft"])
+              basicNft = await ethers.getContract("BasicNft")
+          })
 
-    describe("Constructor", () => {
-        inputToConfig("Initialized the NFT Correctly.", async () => {
-            const name = await basicNft.name()
-            const symbol = await basicNft.symbol()
-            const tokenCounter = await basicNft.getTokenCounter()
-            assert.equal(name, "Dogie")
-            assert.equal(symbol, "DOG")
-            assert.equal(tokenCounter.toString(), "0")
-        })
-    })
-})
+          describe("Constructor", () => {
+              it("Initialized the NFT Correctly.", async () => {
+                  const name = await basicNft.name()
+                  const symbol = await basicNft.symbol()
+                  const tokenCounter = await basicNft.getTokenCounter()
+                  assert.equal(name, "Dogie")
+                  assert.equal(symbol, "DOG")
+                  assert.equal(tokenCounter.toString(), "0")
+              })
+          })
+
+          describe("Mint NFT", () => {
+              it("Allows users to mint an NFT, and updates appropriately", async function () {
+                  const txResponse = await basicNft.mintNft()
+                  await txResponse.wait(1)
+                  const tokenURI = await basicNft.tokenURI(0)
+                  const tokenCounter = await basicNft.getTokenCounter()
+
+                  assert.equal(tokenCounter.toString(), "1")
+                  assert.equal(tokenURI, await basicNft.TOKEN_URI())
+              })
+          })
+      })
