4099283b1145c7aa75077b789fa1bd9fd47c4650
Owanikin <oderindeife@gmail.com>
2022-10-24 08:57:35 +0100

    random generation & pinata

--- deploy/02-deploy-random-ipfs.js
+++ deploy/02-deploy-random-ipfs.js
@@ -1,11 +1,18 @@
 const { network, ethers } = require("hardhat")
 const { developmentChains, networkConfig } = require("../helper-hardhat-config")
 const { verify } = require("../utils/verify")
+const { storeImages } = require("../utils/uploadToPinata")
+
+const imagesLocation = "images/randomNft"
 
 module.exports = async function ({ getNamedAccounts, deployments }) {
     const { deploy, log } = deployments
     const { deployer } = await getNamedAccounts()
     const chainId = network.config.chainId;
+    let tokenUris
+    if (process.env.UPLOAD_TO_PINATA == "true") {
+        tokenUris = await handleTokenUris()
+    }
 
     let vrfCoordinatorV2Address, subscriptionId
 
@@ -21,11 +28,22 @@ module.exports = async function ({ getNamedAccounts, deployments }) {
     }
 
     log("-----------------------")
-    const args = [
-        vrfCoordinatorV2Address,
-        subscriptionId,
-        networkConfig[chainId].gasLane,
-        networkConfig[chainId].callbackGasLimit,
-        networkConfig[chainId].mintFee,
-    ]
+    await storeImages(imagesLocation)
+    // const args = [
+    //     vrfCoordinatorV2Address,
+    //     subscriptionId,
+    //     networkConfig[chainId].gasLane,
+    //     networkConfig[chainId].callbackGasLimit,
+    //     networkConfig[chainId].mintFee,
+    // ]
 }
+
+async function handleTokenUris() {
+    tokenUris = []
+    // store the image in IPFS
+    // store the metadata is IPFS
+
+    return tokenUris
+}
+
+module.exports.tags = ["all", "randomipfs", "main"]
