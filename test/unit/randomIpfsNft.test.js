26d43fb6eeda4b2701b1bff2c0fc1ea691c935a7
Owanikin <oderindeife@gmail.com>
2022-10-25 12:33:51 +0100

    test: constructor

---
+++ test/unit/randomIpfsNft.test.js
@@ -0,0 +1,26 @@
+const { inputToConfig } = require("@ethereum-waffle/compiler")
+const { assert, expect } = require("chai")
+const { network, deployments, ethers } = require("hardhat")
+const { developmentChains, networkConfig } = require("../../helper-hardhat-config")
+
+!developmentChains.includes(network.name)
+    ? describe.skip
+    : describe("Random IPFS NFT Unit Tests", async function () {
+          let randomIpfsNft, deployer, vrfCoordinatorV2Mock
+
+          beforeEach(async () => {
+              accounts = await ethers.getSigners()
+              deployer = accounts[0]
+              await deployments.fixture(["mocks", "randomipfs"])
+              randomIpfsNft = await ethers.getContract("RandomIpfsNft")
+              vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock")
+          })
+
+          describe("constructor", () => {
+              inputToConfig("sets starting values correctly", async function () {
+                  const dogTokenUriZero = await randomIpfsNft.getDogTokenUris(0)
+                  assert(dogTokenUriZero.includes("ipfs://"))
+                  assert.equal(isInitilized, true)
+              })
+          })
+      })
