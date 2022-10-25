5b7746e573c4fcb963f8efc3851593073db321a7
Owanikin <oderindeife@gmail.com>
2022-10-25 13:07:28 +0100

    test: fulfillRandomWords

--- test/unit/randomIpfsNft.test.js
+++ test/unit/randomIpfsNft.test.js
@@ -1,5 +1,6 @@
 const { assert, expect } = require("chai")
 const { network, deployments, ethers } = require("hardhat")
+const { resolve } = require("path")
 const { developmentChains, networkConfig } = require("../../helper-hardhat-config")
 
 !developmentChains.includes(network.name)
@@ -45,5 +46,37 @@ const { developmentChains, networkConfig } = require("../../helper-hardhat-confi
                   )
               })
           })
-          
+
+          describe("fulfillRandomWords", () => {
+              it("mints NFT after random number is returned", async function () {
+                  await new Promise(async (resolve, reject) => {
+                      randomIpfsNft.once("NftMinted", async () => {
+                          try {
+                              const tokenUri = await randomIpfsNft.tokenURI("0")
+                              const tokenCounter = await randomIpfsNft.getTokenCounter()
+                              assert.equal(tokenUri.toString().includes("ipfs://"), true)
+                              assert.equal(tokenCounter.toString(), "1")
+                              resolve()
+                          } catch (e) {
+                              console.log(e)
+                              reject(e)
+                          }
+                      })
+                      try {
+                          const fee = await randomIpfsNft.getMintFee()
+                          const requestNftResponse = await randomIpfsNft.requestNft({
+                              value: fee.toString(),
+                          })
+                          const requestNftReceipt = await requestNftResponse.wait(1)
+                          await vrfCoordinatorV2Mock.fulfillRandomWords(
+                              requestNftReceipt.events[1].args.requestId,
+                              randomIpfsNft.address
+                          )
+                      } catch (e) {
+                          console.log(e)
+                          reject(e)
+                      }
+                  })
+              })
+          })
       })
