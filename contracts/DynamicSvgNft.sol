438d8fbe2767d935f92c5193eb6410f41ffc9fb5
Owanikin <oderindeife@gmail.com>
2022-10-25 15:41:06 +0100

    Dynamic SVG Contract

---
+++ contracts/DynamicSvgNft.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract DynamicSvgNft is ERC721 {
+    // mint
+    // store out SVG information somewhere
+    // Some logic to say "Show X Image" or "Show Y Image"
+    uint256 private s_tokenCounter;
+    string private i_lowImageURI;
+    string private i_highImageURI;
+
+    constructor(string memory lowSvg, string memory highSvg) ERC721("Dynamic SVG NFT", "DSN") {
+        s_tokenCounter = 0;
+    }
+
+    function mintNft() public {
+        _safeMint((msg.sender), s_tokenCounter);
+        s_tokenCounter = s_tokenCounter = 1;
+    }
+}
