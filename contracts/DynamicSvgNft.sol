798452384540f0f22de5f18d12a6f33b013d3c58
Owanikin <oderindeife@gmail.com>
2022-11-03 15:30:35 +0100

    concatenate

--- contracts/DynamicSvgNft.sol
+++ contracts/DynamicSvgNft.sol
@@ -27,20 +27,30 @@ contract DynamicSvgNft is ERC721 {
         s_tokenCounter = s_tokenCounter = 1;
     }
 
+    function _baseURI() internal pure override returns (string memory) {
+        return "data:application/json;base64,";
+    }
+
     function tokenURI(uint256 tokenId) public view override returns (string memory) {
         require(_exists(tokenId), "URI Query for nonexistent token");
         string memory imageURI = "hi!";
-        Base64.encode(
-            bytes(
+        return
+            string(
                 abi.encodePacked(
-                    '{"name":"',
-                    name(),
-                    '", "description":"An NFT that changes based on the Chainlink Feed",',
-                    '"attributes": [{"trait_type": "coolness", "value": 100}], "image":"',
-                    imageURI,
-                    '"}'
+                    _baseURI(),
+                    Base64.encode(
+                        bytes(
+                            abi.encodePacked(
+                                '{"name":"',
+                                name(),
+                                '", "description":"An NFT that changes based on the Chainlink Feed",',
+                                '"attributes": [{"trait_type": "coolness", "value": 100}], "image":"',
+                                imageURI,
+                                '"}'
+                            )
+                        )
+                    )
                 )
-            )
-        );
+            );
     }
 }
