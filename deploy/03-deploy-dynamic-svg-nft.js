deb67a4781f2d6f2869cfa9f8a1d3de1649e4ad4
Owanikin <oderindeife@gmail.com>
2022-11-03 16:07:55 +0100

    deploy script

---
+++ deploy/03-deploy-dynamic-svg-nft.js
@@ -0,0 +1,8 @@
+const { network } = require("hardhat")
+const { developmentChains } = require("../helper-hardhat-config")
+const { verify } = require("../utils/verify")
+
+module.exports = async function ({ getNamedAccounts, deployments }) {
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+}
